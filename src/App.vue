3a53a5461f21b42d6b6028bc4cdd1f9d3ad30bc2
wzw <1541751577@qq.com>
2022-08-31 21:51:51 +0800

    登录页面基本布局完成

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,6 @@
 <template>
   <div id="app">
     <router-view/>
-    <h1>头条</h1>
-    <i class="toutiao toutiao-shouye"></i>
-    <br>
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button>
-    <van-cell-group>
-      <van-cell title="单元格" value="内容" />
-      <van-cell title="单元格" value="内容" label="描述信息" />
-    </van-cell-group>
-    <div class="box">123</div>
-    <div class="box1">456</div>
   </div>
 </template>
 
@@ -26,14 +12,5 @@ export default {
 </script>
 
 <style lang="less">
-  .box {
-    width: 750px;
-    height: 80px;
-    background-color: pink;
-  }
-  .box1 {
-    width: 375px;
-    height: 80px;
-    background-color: green;
-  }
+
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,11 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [
-
+  {
+    name: 'login',
+    path: '/login',
+    component: () => import('@/views/login')
+  }
 ]
 
 const router = new VueRouter({
